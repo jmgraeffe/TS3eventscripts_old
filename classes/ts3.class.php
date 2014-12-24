cd435bb4da1fadf0936d1494835faa6731ea7070
Kandru.de <Support@Kandru.de>
2014-12-24 16:40:55 +0100

    ts3 class optimization
    optimized names of functions, commenting functions

--- classes/ts3.class.php
+++ classes/ts3.class.php
@@ -24,8 +24,26 @@
   SOFTWARE.
  */
 
+/*
+ * EVts3
+ * --------------------------------
+ * This class contains all the stuff for
+ * teamspeak server communication. It's not
+ * finished yet but will get updated from time
+ * to time. Feel free to contribute your changes.
+ * 
+ * Developers need to check the "answer" of the ts3server
+ * manually.
+ */
+
 class EVts3 {
     /* helper functions */
+
+    /*
+     * unescape text that we have got from teamspeak
+     * --------------------------------
+     * thanks to ts3admin.info :)
+     */
 
     public static function unEscapeText($text) {
         $escapedChars = array("\t", "\v", "\r", "\n", "\f", "\s", "\p", "\/");
@@ -33,6 +51,12 @@ class EVts3 {
         $text = str_replace($escapedChars, $unEscapedChars, $text);
         return $text;
     }
+
+    /*
+     * escape text that we send to teamspeak
+     * --------------------------------
+     * thanks to ts3admin.info :)
+     */
 
     public static function escapeText($text) {
         $text = str_replace("\t", '\t', $text);
@@ -48,7 +72,14 @@ class EVts3 {
 
     /* teamspeak commands */
 
-    public static function ev_regevent($type, $id = '') {
+    /*
+     * register an event for teamspeak
+     * --------------------------------
+     * @type    = type of event
+     * [@id]    = id of event
+     */
+
+    public static function regevent($type, $id = '') {
         $tmp = 'servernotifyregister event=' . $type;
         if (!empty($id)) {
             $tmp.= ' id=' . $id;
@@ -56,34 +87,80 @@ class EVts3 {
         EVsocket::send_events($tmp);
     }
 
-    public static function ev_useserver($id) {
+    /*
+     * sets the active virtual ts3 server
+     * --------------------------------
+     * @id      = id of virtual server
+     */
+
+    public static function useserver($id) {
         $tmp = 'use sid=' . $id;
         EVsocket::send($tmp);
         EVsocket::send_events($tmp);
     }
 
-    public static function ev_updatenick($name) {
+    /*
+     * set the nickname of server query
+     * --------------------------------
+     * @name    = nickname of server query
+     */
+
+    public static function updatenick($name) {
         $tmp = 'clientupdate client_nickname=' . self::escapeText($name);
         EVsocket::send($tmp);
         EVsocket::send_events($tmp);
     }
 
-    public static function ev_sendmessage($targetmode, $target, $msg) {
+    /*
+     * send a message to a specific target
+     * --------------------------------
+     * @targetmode  = mode of message
+     * @target      = target
+     * @msg         = message
+     */
+
+    public static function sendmessage($targetmode, $target, $msg) {
         $msg = str_replace(' ', '\s', $msg);
         $tmp = 'sendtextmessage targetmode=' . $targetmode . ' target=' . $target . ' msg=' . self::escapeText($msg);
         EVsocket::send($tmp);
     }
 
-    public static function ev_clientList($params = null) {
+    /*
+     * get the list of connected clients
+     * --------------------------------
+     * [@params]    =   get userlist with some parameters
+     */
+
+    public static function clientList($params = null) {
         if (!empty($params)) {
             $params = ' ' . $params;
         }
         $tmp = 'clientlist' . $params;
         return EVsocket::send($tmp, true);
     }
-    
-    public static function ev_clientMove($clid,$cid,$cpw = '') {
-        $tmp = 'clientmove clid='.$clid.' cid='.$cid.(!empty($cpw) ? ' cpw='.self::escapeText($cpw) : '');
+
+    /*
+     * move client to other channel
+     * --------------------------------
+     * @clid    = user id of client
+     * @cid     = channel id
+     * [@cpw]   = password for channel
+     */
+
+    public static function clientMove($clid, $cid, $cpw = '') {
+        $tmp = 'clientmove clid=' . $clid . ' cid=' . $cid . (!empty($cpw) ? ' cpw=' . self::escapeText($cpw) : '');
+        return EVsocket::send($tmp, true);
+    }
+
+    /*
+     * poke specific client
+     * --------------------------------
+     * @clid    = user id of client
+     * @msg     = message for poke
+     */
+
+    public static function clientPoke($clid, $msg) {
+        $tmp = 'clientpoke clid=' . $clid . ' msg=' . self::escapeText($msg);
         return EVsocket::send($tmp, true);
     }
 
