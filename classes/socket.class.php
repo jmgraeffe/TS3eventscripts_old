7dadb3ec539b97e942c7f99eed774128c323dc22
Kandru.de <Support@Kandru.de>
2014-12-24 16:41:26 +0100

    socket class optimization
    optimized names of functions, commenting functions

--- classes/socket.class.php
+++ classes/socket.class.php
@@ -24,15 +24,36 @@
   SOFTWARE.
  */
 
+/*
+ * EVsocket
+ * --------------------------------
+ * This class creates the connection between teamspeak
+ * and TS3eventscripts. It provides two sockets, one for
+ * all event stuff (non blocking) and another (blocking) 
+ * for all other stuff like poking, messaging, ...
+ * 
+ * Please not that you maybe need to whitelist your IP
+ * from the TS3eventscripts server or otherwise you
+ * will get ip banned because of flooding.
+ */
+
 class EVsocket {
 
     private static $socket_events;
     private static $socket;
     private static $instance;
+    private static $maxlength = -1;
 
     public function __construct($ip, $port) {
         self::connect($ip, $port);
     }
+
+    /*
+     * create / get instance of socket class
+     * --------------------------------
+     * @ip      = ip of teamspeak server query
+     * @port    = port of teamspeak server query
+     */
 
     public static function getInstance($ip = '127.0.0.1', $port = 10011) {
         if (is_null(self::$instance)) {
@@ -41,9 +62,16 @@ class EVsocket {
         return self::$instance;
     }
 
+    /*
+     * connection to teamspeak server query
+     * --------------------------------
+     * @ip      = ip of teamspeak server query
+     * @port    = port of teamspeak server query
+     */
+
     private static function connect($ip, $port) {
         $address = "tcp://" . $ip . ":" . $port;
-        $timeout = 1;
+        $timeout = 0.5;
         self::$socket_events = @stream_socket_client($address, $errno, $errstr, $timeout);
         self::$socket = @stream_socket_client($address, $errno, $errstr, $timeout);
         if (self::$socket === false || self::$socket_events === false) {
@@ -56,30 +84,68 @@ class EVsocket {
         EVmain::log('connected to teamspeak3 server query');
     }
 
+    /*
+     * login as server query admin
+     * --------------------------------
+     * @user    = username of serverquery
+     * @pass    = password of serverquery
+     */
+
     public static function login($user, $pass) {
         self::send('login ' . $user . ' ' . $pass);
         self::send_events('login ' . $user . ' ' . $pass);
     }
 
+    /*
+     * send message to events socket
+     * --------------------------------
+     * @msg     = message
+     */
+
     public static function send_events($msg) {
         stream_socket_sendto(self::$socket_events, $msg . "\n");
     }
 
+    /*
+     * send message to socket
+     * --------------------------------
+     * @msg     = message
+     */
+
     public static function send($msg) {
         stream_socket_sendto(self::$socket, $msg . "\n");
-        return trim(@stream_get_contents(self::$socket, 4096));
-    }
-    
-    public static function get_events() {
-        return trim(@stream_get_contents(self::$socket_events, 4096));
+        return trim(@stream_get_contents(self::$socket, self::$maxlength));
     }
 
-    public static function get() {
-        return trim(@stream_get_contents(self::$socket, 4096));
+    /*
+     * get answer from events socket
+     * --------------------------------
+     * 
+     */
+
+    public static function get_events() {
+        return trim(@stream_get_contents(self::$socket_events, self::$maxlength));
     }
+
+    /*
+     * get answer from socket
+     * --------------------------------
+     * 
+     */
+
+    public static function get() {
+        return trim(@stream_get_contents(self::$socket, self::$maxlength));
+    }
+
+    /*
+     * close socket connection
+     * --------------------------------
+     * 
+     */
 
     public static function close() {
         fclose(self::$socket);
+        fclose(self::$socket_events);
     }
 
 }
