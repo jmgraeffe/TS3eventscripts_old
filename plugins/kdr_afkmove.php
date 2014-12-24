8dbf8d264ced53032ac038df5c330cd7c409c8c0
Kandru.de <Support@Kandru.de>
2014-12-24 16:42:16 +0100

    Official AFKmove Plugin updated
    Updated the official AFKmove Plugin to be compatible with the latest
    version of TS3eventscripts

--- plugins/kdr_afkmove.php
+++ plugins/kdr_afkmove.php
@@ -24,37 +24,59 @@
   SOFTWARE.
  */
 
+/*
+ * AFKmove
+ * --------------------------------
+ * This plugin moves everybody with
+ * muted headphones to an specific
+ * channel and back after unmute.
+ * 
+ * easy to use - just specify the
+ * afk room ID and proof interval.
+ * Do not set it below 3! Otherwise
+ * it could affect other plugins on
+ * full servers.
+ */
+
+/*
+ * Version History
+ * --------------------------------
+ * v0.1
+ *      + added proof interval
+ *      + 
+ */
+
 class kdr_afkmove {
 
-    private $clientlist = array();
-    private $afk_room_id = 2940;
+    private $afk_room_id = 0;
+    private $proof_interval = 10;
     private $afklist = array();
+    private $timer = 0;
 
-    public function __construct() {
-        // listen to disconnecting user
-        EVts3::ev_regevent('server');
-        // set timer
-        $this->timer['clientlist'] = 0;
+    public function __construct($options) {
+        // set options
+        $this->afk_room_id = $options['afkroom'];
+        $this->proof_interval = $options['interval'];
+        // register event and listen for joining / leaving people
+        EVts3::regevent('server');
+        // register global clientList
+        EVmain::setClientList($this->proof_interval);
     }
 
     public function loop($array) {
-        // if a client disconnects we have to clear the array entry
-        if(!empty($array['msg'])) {
+        // remove client from afklist if he disconnects
+        if (!empty($array['msg'])) {
             $array = EVmain::parse($array['msg']);
-            if(isset($array['ev_type']) && $array['ev_type'] == 'notifyclientleftview') {
+            if (isset($array['type']) && $array['type'] == 'notifyclientleftview') {
                 unset($this->afklist[$array['clid']]);
             }
         }
-        // timer
-        foreach ($this->timer as $key => $time) {
-            if ($time <= time() - 5) {
-                $this->timer[$key] = time();
-                switch ($key) {
-                    case 'clientlist':
-                        $this->proof_move(EVmain::parse(EVts3::ev_clientList('-groups -voice -away -uid -info -country')));
-                        break;
-                }
-            }
+        // if we can execute the next proof for clients
+        if ($this->timer <= time() - $this->proof_interval) {
+            // get clientList and execute function proof_move
+            $this->proof_move(EVmain::$clientList);
+            // set timer to now
+            $this->timer = time();
         }
     }
 
@@ -65,28 +87,36 @@ class kdr_afkmove {
     private function proof_move($array) {
         //print_r($array);
         foreach ($array as $user) {
-            // only switch real users
-            if ($user['client_type'] == 0) {
+            // only switch real users and if client id exists
+            if (isset($user['client_type']) && $user['client_type'] == 0 && isset($user['clid'])) {
+                // if headphones muted execute movafk otherwise moveback
                 if ($user['client_output_muted'] == 1) {
                     $this->moveAfk($user);
-                }else{
+                } else {
                     $this->moveBack($user);
                 }
             }
         }
     }
-    
+
     private function moveAfk($user) {
-        if(!isset($this->afklist[$user['clid']])) {
-            EVts3::ev_clientMove($user['clid'],$this->afk_room_id);
+        // if the user isn't already afk
+        if (!isset($this->afklist[$user['clid']])) {
+            // move him to afk room
+            EVts3::clientMove($user['clid'], $this->afk_room_id);
+            // add him to the afklist
             $this->afklist[$user['clid']] = $user['cid'];
         }
     }
-    
+
     private function moveBack($user) {
-        if(isset($this->afklist[$user['clid']])) {
-            EVts3::ev_clientMove($user['clid'],$this->afklist[$user['clid']]);
+        // if the user was AFK and isn't any more
+        if (isset($this->afklist[$user['clid']])) {
+            // move back to old channel
+            EVts3::clientMove($user['clid'], $this->afklist[$user['clid']]);
+            // remove from list
             unset($this->afklist[$user['clid']]);
         }
     }
+
 }
